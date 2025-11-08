2cdec4da8569c4768cf002be026b9bb45e74ca22
Elizabeth Jeudy <elizabethjeudy@Elizabeths-MacBook-Air.local>
2025-11-08 10:40:01 -0500

    updated with nested condition

--- rollercoaster-height.py
+++ rollercoaster-height.py
@@ -2,7 +2,14 @@ print("Weclome to the rollercoaster!")
 
 height = int(input("What is your height in cm?\n "))
 
-if height == 120:
+if height >= 120:
     print("Yay, you can ride the rollercoaster!")
+    age = int(input("What is your age?\n "))
+    if age <= 12:
+        print("Please pay $5.")
+    elif age <= 18:
+        print("Please pay $7.")
+    else:
+        print("Please pay $12.")
 else:
     print("Sorry, you have to grow taller before you can ride.")
