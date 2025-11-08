69b45b90c780459c0b13fd3ca4e565c2ea6a6687
Elizabeth Jeudy <elizabethjeudy@Elizabeths-MacBook-Air.local>
2025-11-07 21:49:53 -0500

    exercise 3

---
+++ odd-or-even.py
@@ -0,0 +1,8 @@
+# check if input is even or odd
+
+number = int(input("Which number do you want to check? "))
+
+if number % 2 == 0:
+    print("This is an even number.")
+else:
+    print("This is not an even number.")
