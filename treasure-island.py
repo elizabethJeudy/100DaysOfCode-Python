ead29a9e2966c8a355739ccc6422394392c07c71
Elizabeth Jeudy <elizabethjeudy@Elizabeths-MacBook-Air.local>
2025-11-11 19:47:41 -0500

    challenge 6

--- treasure-island.py
+++ treasure-island.py
@@ -28,3 +28,23 @@ print('''
        ''')
 
 print("Welcome to the incredible Treasure Island!\n Your mission is simple: find the treasure or die trying!")
+
+choice1 = input(
+    "You are running away from the police and come between 2 paths to take. Do you go left or right?\n ").lower()
+
+
+if choice1 == "left":
+    choice2 = input(
+        "The path you chose has lead you to a lake. Do you swim across or hope that someone with a boat will pass by? Type swim or wait\n").lower()
+    if choice2 == "wait":
+        choice3 = input(
+            "Someone rescused you and you made it across the lake saftely and see a house with 3 doors: one red, one yellow, and one blue. Which door are you walking through to discover your fate?\n").lower()
+        if choice3 != "yellow":
+            print(
+                "You enter a room of beasts. They didn't like you trespassing on their property. Busted!")
+        else:
+            print("You found the treasure! You win!")
+    else:
+        print("You swam across the lake but were bitten by an alligator, and surprisingly the police were waiting for you on the other side. Busted!")
+else:
+    print("As you were running down this path, you fell into a sinkhole and the police caught you. Busted!")
